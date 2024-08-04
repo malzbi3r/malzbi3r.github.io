7324127e1b3ec68e45f1981f75ef94a3f14f8c0f
malzbi3r <contato.cuervo279@gmail.com>
2024-08-04 19:38:29 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
 function changeSrc(newSrc) {
-    document.getElementById("myIframe").src = `https://playertv.net/e/?v=${newSrc}`;
+    document.getElementById("myIframe").src = `https://playertv.net/e/?v=${newSrc}&autoplay=true`;
+    iframe.load();
   }
